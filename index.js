5efbd97b496b9e05fc37367405d528c6eccec02a
juan <damorenor@unal.edu.co>
2020-08-23 22:45:47 -0500

    bot key added

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ const Discord = require('discord.js');
 
 const client  = new Discord.Client();
 
+client.once('ready', () =>{
+    console.log('disCF is online');
+});
+
+client.login('NzQ3Mjk2MzY1NjEyMzY3ODg0.X0Mz-Q.SoSJiRX9LeCeOeKR31sRL-TuvGI');
